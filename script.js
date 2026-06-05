31032fce756efdf489153c6b1237d89d9870f18a
Mohd. Sameer <66036809+Sameer-Shahzada@users.noreply.github.com>
2026-06-05 21:43:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-//your JS code here. If required.
-const form = document.getElementById("myForm");
-
-form.addEventListener("submit", function (e) {
+document.getElementById("btn").addEventListener("click", function (e) {
     e.preventDefault();
 
     const age = document.getElementById("age").value;
     const name = document.getElementById("name").value;
 
     if (!age || !name) {
-        alert("Please enter valid details.");
+        alert("Please enter valid details");
         return;
     }
 
-    const votingPromise = new Promise((resolve, reject) => {
+    const promise = new Promise((resolve, reject) => {
         setTimeout(() => {
             if (Number(age) > 18) {
                 resolve(`Welcome, ${name}. You can vote.`);
@@ -22,11 +19,7 @@ form.addEventListener("submit", function (e) {
         }, 4000);
     });
 
-    votingPromise
-        .then((message) => {
-            alert(message);
-        })
-        .catch((message) => {
-            alert(message);
-        });
+    promise
+        .then((msg) => alert(msg))
+        .catch((msg) => alert(msg));
 });
